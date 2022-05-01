4584bb0c944de2e4190e25a36a0ea32ee4588c2f
inoy <yskinee@gmail.com>
2022-05-01 20:38:59 +0900

    876. Middle of the Linked List

---
+++ 876.ts
@@ -0,0 +1,43 @@
+// Definition for singly-linked list.
+class ListNode {
+  val: number
+  next: ListNode | null
+
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+  }
+}
+
+const _middleNode = (head: ListNode | null, length: number): number => {
+  const {next} = head
+  if (next == null) return length
+  return _middleNode(next, ++length)
+}
+
+function middleNode(head: ListNode | null): ListNode | null {
+  if (head == null) return null
+  let {next} = head
+  if (next == null) return head
+  const length = _middleNode(head, 1)
+  const middle = Math.round((length - 1) / 2)
+  console.log(`cnt = ${length}, middle = ${middle}`)
+  for (let i = 0; i < middle - 1; i++) {
+    next = next.next
+  }
+  return next
+}
+
+console.log((middleNode(
+  new ListNode(1, null
+    // new ListNode(2,
+    //   new ListNode(3,
+    //     new ListNode(4,
+    //       new ListNode(5,
+    //         new ListNode(6,
+    //           null))
+    //     )
+    //   )
+    // )
+  )
+)))
