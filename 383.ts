5bee13be6d0b2be5229acef34a0fd82d4fc889d6
inoy <yskinee@gmail.com>
2022-05-01 10:25:04 +0900

    383. Ransom Note

---
+++ 383.ts
@@ -0,0 +1,15 @@
+function canConstruct(ransomNote: string, magazine: string): boolean {
+  for (let i = 0; i < ransomNote.length; i++) {
+    const n = ransomNote[i]
+    // console.log(`n = ${n}`)
+    // console.log(`magazine = ${magazine}`)
+
+    const a = magazine.indexOf(n)
+    // console.log(`a = ${a}`)
+    if (a === -1) return false
+    magazine = magazine.slice(0, a) + magazine.slice(a + 1)
+  }
+  return true
+}
+
+console.log(canConstruct('aa', 'aab'))
