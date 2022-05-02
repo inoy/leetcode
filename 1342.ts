e33a0955f02fcb2460ed58e87bf519ae17cc64aa
inoy <yskinee@gmail.com>
2022-05-02 10:41:44 +0900

    1342. Number of Steps to Reduce a Number to Zero

---
+++ 1342.ts
@@ -0,0 +1,12 @@
+const _numberOfSteps = (num, step: number): number => {
+  if (num === 0) return step
+  const half = num / 2
+  if (Number.isInteger(half)) return _numberOfSteps(half, ++step)
+  return _numberOfSteps(--num, ++step)
+}
+
+function numberOfSteps(num: number): number {
+  return _numberOfSteps(num, 0)
+}
+
+console.log(numberOfSteps(14))
