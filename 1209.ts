ac3be119e04e24d91232971c0ba6473730c7d46d
inoy <7m7110123@gmail.com>
2022-05-06 23:19:53 +0900

    1209. Remove All Adjacent Duplicates in String II

---
+++ 1209.ts
@@ -0,0 +1,23 @@
+export {}
+
+function removeDuplicates(s: string, k: number): string {
+  const stack: [string, number][] = []
+  for (const char of s) {
+    const last = stack.at(-1)
+    if (last !== undefined && last[0] === char) {
+      if (++last[1] === k) stack.pop()
+    } else {
+      stack.push([char, 1])
+    }
+  }
+
+  let ans = ''
+  for (const [char, count] of stack) {
+    ans += char.repeat(count)
+  }
+  return ans
+}
+
+console.log(removeDuplicates('abcd', 2))
+console.log(removeDuplicates('deeedbbcccbdaa', 3))
+console.log(removeDuplicates('pbbcggttciiippooaais', 2))
