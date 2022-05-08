b50655e7e243ebb84c9e00d3194eed872c7a7421
inoy <yskinee@gmail.com>
2022-05-09 08:57:35 +0900

    541. Reverse String II

---
+++ 541.ts
@@ -0,0 +1,15 @@
+export {}
+
+function reverseStr(s: string, k: number): string {
+  const ss = s.split('')
+  for (let i = 0; i < ss.length; i += 2 * k) {
+    const reverse = ss.splice(i, k).reverse()
+    console.log({reverse})
+    ss.splice(i, 0, ...reverse)
+    console.log({ss})
+  }
+
+  return ss.join('')
+}
+
+console.log(reverseStr('abcdefg', 2))
