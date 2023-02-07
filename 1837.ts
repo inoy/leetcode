15b3f694345c8fd5210e8a42e326e81c64e5b911
inoy <yskinee@gmail.com>
2023-02-07 21:54:04 +0900

    1837. Sum of Digits in Base K

---
+++ 1837.ts
@@ -0,0 +1,12 @@
+export {}
+
+function sumBase(n: number, k: number): number {
+  let sum = 0
+  while (n > 0) {
+    sum += n % k
+    n = Math.floor(n / k)
+  }
+  return sum
+}
+
+console.log(sumBase(34, 6))
